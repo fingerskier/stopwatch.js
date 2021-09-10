28274b8ed0b464b2d060a0380d2d934cb179d3b6
fingerskier <fingerskier@gmail.com>
2021-09-10 13:36:40 -0500

    basic tests

--- index.test.js
+++ index.test.js
@@ -2,47 +2,133 @@ const Gnomon = require('.')
 const assert = require('assert')
 
 
+// T0
 async function basic_test() {
   const clepsydra = new Gnomon({
     interval: 250,
     
-    onTick: console.log,
+    onTick: res=>{
+      console.log(`T0 ${res.count}`)
+    },
   })
   
   
   clepsydra.start()
+
+  console.log('T0', clepsydra.timer)
   
   setTimeout(() => {
     clepsydra.stop(res=>{
       console.log('T0 stopped', res)
 
-      assert(res.count === 3)
+      assert(res.count === 9)
     })
   }, 2345);  
 }
+// T0
 
 
+// T1
 async function tick_test() {
   const clepsydra = new Gnomon({
     interval: 345,
     
-    onTick: res=>console.log(res),
+    onTick: res=>{
+      console.log(`T1 ${res.count}`)
+    },
   })
   
   
   clepsydra.start()
+
+  console.log('T1', clepsydra.timer)
+
   
   setTimeout(() => {
-    clepsydra.stop(res=>console.log('T1 stopped', res))
+    clepsydra.stop(res=>{
+      console.log('T1 stopped', res)
+      
+      assert(res.count === 3)
+    })
   }, 1234);
-  
 }
+// T1
 
 
+// T2
+async function stop_test() {
+  let stopCount = 0
 
+  const clepsydra = new Gnomon({
+    interval: 500,
+
+    onElapsed: res=>{
+      stopCount++
+    },
+    
+    onTick: res=>{
+      console.log(`T2 ${res.count}`)
+    },
+
+    timeout: 3456,
+  })
+  
+  
+  clepsydra.start()
+
+  console.log('T2', clepsydra.timer)
+
+  
+  setTimeout(() => {
+    clepsydra.stop(res=>{
+      console.log('T2 stopped', res)
+      
+      assert((res.count === 6) && !stopCount)
+    })
+  }, 3400);
+}
+// T2
+
+
+// T3
+async function elapsed_test() {
+  let stopCount = 0
+
+  const clepsydra = new Gnomon({
+    interval: 500,
+
+    onElapsed: res=>{
+      stopCount++
+      console.log('T3 elapsed', stopCount)
+    },
+    
+    onTick: res=>{
+      console.log(`T3 ${res.count}`)
+    },
+
+    timeout: 3500,
+  })
+  
+  
+  clepsydra.start()
+
+  console.log('T3', clepsydra.timer)
+
+  
+  setTimeout(() => {
+    clepsydra.stop(res=>{
+      console.log('T3 stopped', res, stopCount)
+      
+      assert((res.count === 7) && (stopCount === 1))
+    })
+  }, 4000);
+}
+// T3
 
 
 Promise.all([
   basic_test(),
   tick_test(),
+  stop_test(),
+  elapsed_test(),
 ]).catch(console.error)
